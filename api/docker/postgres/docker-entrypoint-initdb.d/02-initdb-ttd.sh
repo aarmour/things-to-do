08d736a0e90399b65ff7f78fb58905b859ed5e4d
Adam Armour <adam.armour@gmail.com>
2016-09-06 20:36:37 -0600

    chore(api/docker): configure PostGIS

--- api/docker/postgres/docker-entrypoint-initdb.d/02-initdb-ttd.sh
+++ api/docker/postgres/docker-entrypoint-initdb.d/02-initdb-ttd.sh
@@ -3,6 +3,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER ttd;
-    CREATE DATABASE ttd;
+    CREATE DATABASE ttd TEMPLATE template_postgis;
     GRANT ALL PRIVILEGES ON DATABASE ttd TO ttd;
 EOSQL
