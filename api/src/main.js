aeb9dc1d531d7a04ef22848170f7136b16bc3b7a
Adam Armour <adam.armour@gmail.com>
2016-09-08 21:58:45 -0600

    fix(api): log message

--- api/src/main.js
+++ api/src/main.js
@@ -4,5 +4,5 @@ const server = require('./app').server;
 
 server.start((error) => {
   if (error) throw error;
-  server.log(`Server running at: ${server.info.uri}`);
+  server.log(['info'], { message: `Server running at: ${server.info.uri}` });
 });
