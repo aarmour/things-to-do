dcc3f006e330f0c4f0dd070676fa484733668417
Adam Armour <adam.armour@gmail.com>
2016-09-09 22:19:50 -0600

    feat(core): bootstrap core module with auth service placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { routing, appRoutingProviders } from './app.routing';
+import { CoreModule } from './core';
 import { DashboardModule } from './dashboard';
 
 @NgModule({
