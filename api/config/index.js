50f5379ddef0e25fd7e51285df795a993eadf1f4
Adam Armour <adam.armour@gmail.com>
2016-09-09 19:20:31 -0600

    feat(api/config): add support for .local
    Attempt to load config file from .local for local development settings that do not get committed.

--- api/config/index.js
+++ api/config/index.js
@@ -8,6 +8,7 @@ const conf = convict(schema);
 const env = conf.get('env');
 
 conf.loadFile(path.resolve(__dirname, `./environment.${env}.json`));
+try { conf.loadFile(path.resolve(__dirname, `../.local/environment.${env}.json`)); } catch(e) {}
 conf.validate({ strict: true });
 
 module.exports = conf.get();
