3e9c2c46720c971472ad52a9405c47c38c23a38e
Adam Armour <adam.armour@gmail.com>
2016-09-25 17:42:35 -0600

    feat(shared/mapbox/info-popup): bind content via component

--- src/app/shared/mapbox/control/info-popup/info-popup.component.ts
+++ src/app/shared/mapbox/control/info-popup/info-popup.component.ts
@@ -1,5 +1,7 @@
 import {
   Component,
+  ViewChild,
+  ElementRef,
   Input
 } from '@angular/core';
 
@@ -14,16 +16,36 @@ import { InfoPopup } from './info-popup.control';
 })
 export class InfoPopupComponent extends ControlComponent {
 
+  private contentObserver: any;
+  private infoPopup: any;
+
   @Input() position: string = 'bottom-left';
+
+  @ViewChild('content') content: ElementRef;
 
   constructor() {
     super();
   }
 
+  ngAfterViewInit() {
+    const observer = this.contentObserver = new MutationObserver((mutations) => this.setContent());
+    observer.observe(this.content.nativeElement.children[0], { childList: true, characterData: true, subtree: true });
+  }
+
   afterSetMap() {
     if (!this.map) return;
-    const infoPopup = new InfoPopup({ position: this.position });
+
+    const infoPopup = this.infoPopup = new InfoPopup({ position: this.position });
+
     this.map.addControl(infoPopup);
+    this.setContent();
+  }
+
+  setContent() {
+    if (!this.content) return;
+    if (!this.infoPopup) return;
+
+    this.infoPopup.html(this.content.nativeElement.children[0].innerHTML);
   }
 
 }
