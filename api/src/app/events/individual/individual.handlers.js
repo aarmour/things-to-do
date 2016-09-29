144c03f6510c3ce932c554c2b6b245f47425d372
Adam Armour <adam.armour@gmail.com>
2016-09-28 20:45:19 -0600

    feat(api/events): create and read an individual event

--- api/src/app/events/individual/individual.handlers.js
+++ api/src/app/events/individual/individual.handlers.js
@@ -1,11 +1,24 @@
 'use strict';
 
+const { notFound } = require('boom');
+
 exports.create = function(request, reply) {
-  reply('OK');
+  const { Event } = this.models;
+
+  Event.create(request.payload)
+    .then((event) => reply(null, event))
+    .catch((error) => reply(error));
 };
 
 exports.read = function(request, reply) {
-  reply('OK');
+  const { Event } = this.models;
+
+  Event.findById(request.params.id)
+    .then((event) => {
+      if (event) reply(null, event);
+      else reply(notFound());
+    })
+    .catch((error) => reply(error));
 };
 
 exports.update = function(request, reply) {
