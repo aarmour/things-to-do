1439bc0d45bc3e879b3a0b1fab4422fea4597181
Adam Armour <adam.armour@gmail.com>
2016-10-15 21:54:54 -0600

    refactor(actions): organize auth actions

--- src/app/core/app-state/actions/auth/login.action.ts
+++ src/app/core/app-state/actions/auth/login.action.ts
@@ -1,7 +1,17 @@
-import { User } from '../models/user.model';
-
 import { Action } from '@ngrx/store';
-import { type } from '../util';
+
+import { User } from '../../models/user.model';
+import { type } from '../../util';
+
+export const LOGIN = type('login');
+
+export class LoginAction implements Action {
+
+  type = LOGIN;
+
+  constructor() {}
+
+}
 
 export const LOGIN_SUCCESS = type('login success');
 
