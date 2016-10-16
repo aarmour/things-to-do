30bc8590d77a9be3c676cc03feec4ac2bf9879fc
Adam Armour <adam.armour@gmail.com>
2016-10-16 14:36:42 -0600

    feat(geocoder-service): add reverseGeocode method

--- src/app/shared/mapbox/geocoder/geocoder.service.ts
+++ src/app/shared/mapbox/geocoder/geocoder.service.ts
@@ -19,20 +19,19 @@ export class GeocoderService {
   constructor(private http: Http) { }
 
   geocode(query: string) {
-    const searchParams = new URLSearchParams();
-    searchParams.set('access_token', mapboxgl.accessToken);
+    return this.makeRequest(query);
+  }
 
-    const requestOptions = new RequestOptions({
-      search: searchParams
-    });
-
-    return this.http.get(this.formatUri(query), requestOptions)
-      .map(this.extractData)
-      .catch(this.handleError);
+  reverseGeocode(point: mapboxgl.LngLat) {
+    return this.makeRequest(point);
   }
 
   private extractData(response: Response) {
     return response.json();
+  }
+
+  private formatPointQuery(point: mapboxgl.LngLat) {
+    return point.toArray().join(',');
   }
 
   private formatUri(query: string) {
@@ -43,6 +42,23 @@ export class GeocoderService {
     console.error(error);
     const errorMessage = 'Service unavailable - please try again in a few minutes';
     return Observable.throw(errorMessage);
+  }
+
+  private makeRequest(query) {
+    const searchParams = new URLSearchParams();
+    searchParams.set('access_token', mapboxgl.accessToken);
+
+    const requestOptions = new RequestOptions({
+      search: searchParams
+    });
+
+    if (query instanceof mapboxgl.LngLat) {
+      query = this.formatPointQuery(query) as string;
+    }
+
+    return this.http.get(this.formatUri(query), requestOptions)
+      .map(this.extractData)
+      .catch(this.handleError);
   }
 
 }
