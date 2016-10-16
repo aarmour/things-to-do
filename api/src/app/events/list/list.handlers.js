33baa81fd7d0372a79822a18eee82587cf284031
Adam Armour <adam.armour@gmail.com>
2016-10-16 07:32:21 -0600

    feat(api): wrap events list in object

--- api/src/app/events/list/list.handlers.js
+++ api/src/app/events/list/list.handlers.js
@@ -6,6 +6,6 @@ exports.view = function(request, reply) {
   const { Event } = this.models;
 
   Event.findAll({ limit: defaults.pageSize })
-    .then((events) => reply(events))
+    .then((events) => reply({ events }))
     .catch(() => reply(new Error()));
 };
